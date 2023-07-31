47c94402729bac50811c8f0b9ef2cfc2981292e1
ejkim000 <developer.ejk@gmail.com>
2023-07-31 09:41:56 -0400

    add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
-import { useState } from 'react'
+import { Route, Routes } from 'react-router-dom';
+import Currencies from './pages/Currencies';
+import Main from './pages/Main';
+import Price from './pages/Price';
 
 function App() {
 
   return (
     <>
-     
+      <Routes>
+        <Route path='/' element={<Main />} />
+        <Route path='/currencies' element={<Currencies />} />
+        <Route path='/price' element={<Price />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
