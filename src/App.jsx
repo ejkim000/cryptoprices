dd09bf9c0baeb1563c8939fc352071959227dc36
ejkim000 <developer.ejk@gmail.com>
2023-07-31 09:53:25 -0400

    Add Nav to app

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import { Route, Routes } from 'react-router-dom';
 import Currencies from './pages/Currencies';
 import Main from './pages/Main';
 import Price from './pages/Price';
+import Nav from './components/Nav';
 
 function App() {
 
   return (
     <>
+    <Nav />
       <Routes>
         <Route path='/' element={<Main />} />
         <Route path='/currencies' element={<Currencies />} />
